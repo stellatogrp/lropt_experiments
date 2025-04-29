f7cc760c53d3d9290140dce5e26a1b23d8ce8a70
Irina <iywang@princeton.edu>
2025-04-29 15:55:21 -0400

    exps

---
+++ slurm/portfolio.sh
@@ -0,0 +1,20 @@
+#!/bin/bash
+#SBATCH --job-name=portfoliotest
+#SBATCH --nodes=1
+#SBATCH --ntasks=1
+#SBATCH --cpus-per-task=35
+#SBATCH --mem-per-cpu=8G
+#SBATCH --time=23:00:00
+#SBATCH -o /scratch/gpfs/iywang/lropt_revision/output/portfolio/portfolio_test_%A_.txt
+#SBATCH --mail-type=BEGIN,END,FAIL,TIME_LIMIT
+#SBATCH --mail-user=iabirina@hotmail.com
+#SBATCH --array=3-4        # job array
+
+module purge
+module load anaconda3/2023.9
+conda activate lropt_rev
+
+python lropt_experiments/port_parallel/port.py 
+#--config-name=port.yaml seed=10 eta=10 
+
+# python portfolio/plot_avg_10.py
