7ac109f82b039661f3e6dfa42d65f8d777365f82
Irina <iywang@princeton.edu>
2025-05-06 20:30:30 -0400

    port

--- slurm/inventory.sh
+++ slurm/inventory.sh
@@ -4,18 +4,18 @@
 #SBATCH --ntasks=1
 #SBATCH --cpus-per-task=15
 #SBATCH --mem-per-cpu=10G
-#SBATCH --time=23:40:00
+#SBATCH --time=1:40:00
 #SBATCH -o /scratch/gpfs/iywang/lropt_revision/output/inventory/inv_test_%A_.txt
 #SBATCH --mail-type=BEGIN,END,FAIL,TIME_LIMIT
 #SBATCH --mail-user=iabirina@hotmail.com
-#SBATCH --array=4        # job array
+#SBATCH --array=0        # job array
 
 module purge
 module load anaconda3/2023.9
 conda activate lropt_rev
 
-# python lropt_experiments/inventory_parallel/inv.py --config-name=inv1.yaml
-python lropt_experiments/inventory_parallel/inv_dro.py --config-name=inv_dro.yaml
+python lropt_experiments/inventory_parallel/inv.py --config-name=inv1.yaml
+# python lropt_experiments/inventory_parallel/inv_dro.py --config-name=inv_dro.yaml
 #--config-name=port.yaml seed=10 eta=10 
 
 # python portfolio/plot_avg_10.py
