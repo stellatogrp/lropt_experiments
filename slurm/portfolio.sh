06895b2b3e2d4c708772f4d4351d920baf86a6d9
Irina <iywang@princeton.edu>
2025-05-12 03:42:44 -0400

    port

--- slurm/portfolio.sh
+++ slurm/portfolio.sh
@@ -3,20 +3,20 @@
 #SBATCH --nodes=1
 #SBATCH --ntasks=1
 #SBATCH --cpus-per-task=18
-#SBATCH --mem-per-cpu=10G
+#SBATCH --mem-per-cpu=20G
 #SBATCH --time=23:40:00
 #SBATCH -o /scratch/gpfs/iywang/lropt_revision/output/portfolio/portfolio_test_%A_.txt
 #SBATCH --mail-type=BEGIN,END,FAIL,TIME_LIMIT
 #SBATCH --mail-user=iabirina@hotmail.com
-#SBATCH --array=0        # job array
+#SBATCH --array=2       # job array
 
 module purge
 module load anaconda3/2023.9
 conda activate lropt_rev
 
-# python lropt_experiments/port_parallel/port.py --config-name=port.yaml
+python lropt_experiments/port_parallel/port.py --config-name=port.yaml
 # python lropt_experiments/port_parallel/port_dro.py --config-name=port_dro.yaml
-python lropt_experiments/port_parallel/port_delage.py --config-name=port_delage.yaml
+# python lropt_experiments/port_parallel/port_delage.py --config-name=port_delage.yaml
 
 #--config-name=port.yaml seed=10 eta=10 
 
